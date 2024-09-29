cbf6ff3a4f28d5fcd27524f77dd2c08f1bf11da1
Maram-Hassan <maram.hassan95@gmail.com>
2024-09-29 10:57:40 +0300

    Initial commit for Siemens task project with Terraform and Ansible

--- output.tf
+++ output.tf
@@ -2,7 +2,7 @@
 output "vpc_id" {
   value = aws_vpc.terraform-vpc.id
 }
-output "instance_ip" {
-  value = aws_instance.your_instance_name.public_ip
-}
 
+output "instance_public_ip" {
+  value = aws_instance.TerraformInstance.public_ip
+}
